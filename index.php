2d8f4bebd9c16b74116d01f366b71668f6a68217
mice <mice@devoffice.com>
2015-09-10 17:28:32 +0300

    зарисовка поля, фишек

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<script src="assets/js/jquery-2.1.4.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/scripts.js"></script>
+	<link rel="stylesheet" href="assets/css/bootstrap.css">
+	<link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+	<link rel="stylesheet" href="assets/css/styles.css">
+</head>
+<body>
+	<div class="content container">
+		<div class="row">
+
+		<div class="main-zone col-lg-9">
+			<div class="game-field ">
+				<div class="card red">1</div>
+				<div class="card black">2</div>
+				<div class="card blue">3</div>
+				<div class="card orange">4</div>
+			</div>
+			<div class="card-pocket ">
+				<div class="card red">13</div>
+			</div>
+		</div>
+		<div class="sidebar col-lg-3">
+
+			<div class="game-controls"></div>
+		</div>
+		</div>
+	</div>
+</body>
+</html>
