062e11290092b94846bb74a543fbee8ece703dba
mice <mice@devoffice.com>
2015-09-11 11:00:57 +0300

    перетаскивание

--- index.php
+++ index.php
@@ -2,9 +2,7 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<script src="assets/js/jquery-2.1.4.js"></script>
-	<script src="assets/js/bootstrap.js"></script>
-	<script src="assets/js/scripts.js"></script>
+
 	<link rel="stylesheet" href="assets/css/bootstrap.css">
 	<link rel="stylesheet" href="assets/css/bootstrap-theme.css">
 	<link rel="stylesheet" href="assets/css/styles.css">
@@ -14,14 +12,15 @@
 		<div class="row">
 
 		<div class="main-zone col-lg-9">
-			<div class="game-field ">
-				<div class="card red">1</div>
-				<div class="card black">2</div>
-				<div class="card blue">3</div>
-				<div class="card orange">4</div>
+			<div class="game-field " id="yo">
+				<div class="card red" draggable="true">1</div>
+				<div class="card black" draggable="true">2</div>
+				<div class="card blue" draggable="true">3</div>
+				<div class="card orange" draggable="true">4</div>
+				<div class="card empty" ></div>
 			</div>
 			<div class="card-pocket ">
-				<div class="card red">13</div>
+				<div class="card red" draggable="true">13</div>
 			</div>
 		</div>
 		<div class="sidebar col-lg-3">
@@ -30,5 +29,10 @@
 		</div>
 		</div>
 	</div>
+
+	<script src="assets/js/jquery-2.1.4.js"></script>
+	<script src="assets/js/bootstrap.js"></script>
+	<script src="assets/js/scripts.js"></script>
+
 </body>
 </html>
